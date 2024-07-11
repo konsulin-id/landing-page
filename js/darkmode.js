f215393d984d76897d04d1718f29fe336c1804e7
lamurian <lamurian@users.noreply.github.com>
2024-07-11 15:03:45 +0000

    deploy: 7a4cd3504b780431053e40806b03a6dc2c824661

--- js/darkmode.js
+++ js/darkmode.js
@@ -2,10 +2,13 @@
   // <stdin>
   var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
   var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
-  if (localStorage.getItem("color-theme") === "dark" || !("color-theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  if (localStorage.getItem("color-theme") === "dark") {
     themeToggleLightIcon.classList.remove("hidden");
+    document.documentElement.classList.add("dark");
   } else {
     themeToggleDarkIcon.classList.remove("hidden");
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("color-theme", "light");
   }
   var themeToggleBtn = document.getElementById("theme-toggle");
   themeToggleBtn.addEventListener("click", function() {
